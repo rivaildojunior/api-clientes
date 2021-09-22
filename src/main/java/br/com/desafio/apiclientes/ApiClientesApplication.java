a63e35d668f39eead0ed26934c5a948908d7fde3
Rivaildo Sousa <rivaildojunior@yahoo.com.br>
2021-09-22 18:58:41 -0300

    Ajustes

--- src/main/java/br/com/desafio/apiclientes/ApiClientesApplication.java
+++ src/main/java/br/com/desafio/apiclientes/ApiClientesApplication.java
@@ -2,11 +2,12 @@ package br.com.desafio.apiclientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class ApiClientesApplication {
+public class ApiClientesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiClientesApplication.class, args);
